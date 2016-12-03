6dec1e445cb8550a1b0f0d05de080e9017c2ca76
Ondřej Mařík <ondra.marik@gmail.com>
2016-12-03 09:14:57 +0100

    Add notification controller

---
+++ kitt/Source/Controller/NotificationVC.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationVC.swift
+//  kitt
+//
+//  Created by Ondřej Mařík on 03/12/2016.
+//  Copyright © 2016 Ondřej Mařík. All rights reserved.
+//
+
+import UIKit
+
+class NotificationVC: BaseVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
